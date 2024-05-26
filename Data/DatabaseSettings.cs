35702a324470064162b84bbce6330a7d7569d1b1
ifaz7ahamed <167524726+ifaz7ahamed@users.noreply.github.com>
2024-05-26 22:04:13 +0530

    first commit

---
+++ Data/DatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Web_App_Mongo.Data
+{
+    public class DatabaseSettings
+    {
+        public string CollectionName {  get; set; }
+        public string DatabaseName { get; set; }
+
+        public string Connection {  get; set; }
+    }
+}
---
+++ Services/StudentServices.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Web_App_Mongo.Data;
+using Web_App_Mongo.Models;
+using Web_App_Mongo.Services;
+
+
+namespace Web_App_Mongo.Services
+{
+    public class StudentServices
+    {
+        private readonly IMongoCollection<Student> _studentCollection;
+
+        public StudentServices(IOptions<DatabaseSettings> settings)
+        {
+            var mongoClient = new MongoClient(settings.Value.Connection);
+            var mongoDb = mongoClient.GetDatabase(settings.Value.DatabaseName);
+            _studentCollection = mongoDb.GetCollection<Student>(settings.Value.DatabaseName);
+        }
+
+        //get all student
+        public async Task<List<Student>> GetAsync() => await _studentCollection.Find(_ => true).ToListAsync();
+
+        //get student by id
+        public async Task<Student> GetAsync(string id) =>
+            await _studentCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        // add new student 
+        public async Task CreateAsync(Student newStudent) =>
+            await _studentCollection.InsertOneAsync(newStudent);
+
+        // update student
+
+        public async Task UpdateAsync(string id, Student updateStudent) =>
+            await _studentCollection.ReplaceOneAsync(x => x.Id == id, updateStudent);
+
+        // delete student
+        public async Task RemoveAsync(string id) =>
+            await _studentCollection.DeleteOneAsync(x => x.Id == id);
+
+    }
+}
